09c229b8dd303c107d77e7fdb564810de2860b33
brigith91 <168245239+brigith91@users.noreply.github.com>
2024-11-14 18:12:40 -0500

    cambios en leer movimientos de jugador

---
+++ elemento.py
@@ -0,0 +1,7 @@
+PARED = '🧱'  # símbolo de pared lleno
+CAJA = '📦'   # símbolo de caja
+DESTINO = '⭕'  # símbolo de destino, como una bandera
+JUGADOR = '🙂'  # símbolo de jugador, como una cara
+ESP_VAC = ' 🕸'  # espacio vacío sin punto
+CAJ_DEST = '🎲'  # caja en destino (puedes combinarlos si quieres)
+JUG_DEST = '👩‍💼'  # jugador en destino
